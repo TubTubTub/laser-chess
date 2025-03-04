baaec30233d422dd8740c0d75d0a42279468ce9c
TubTubTub <tobykwmok@gmail.com>
2025-03-04 09:33:16 +0000

    d

--- documentation/source/input_script.py
+++ documentation/source/input_script.py
@@ -6,9 +6,20 @@ with open('documentation/source/input_lines.txt', 'w') as f:
     for dir_path, dir_names, file_names in root.walk():
         index = str(dir_path).index('data')
         path_suffix = str(dir_path)[index:]
+
+        if len(file_names) > 0 and dir_path.name != '__pycache__':
+            f.write(f'\section{{{path_suffix}}}\n')
         
         for file in file_names:
             file_path = f'..\..\{path_suffix}\{file}'.replace('\\', '/')
+            extension = file.split('.')[1]
+            depth = len(dir_path.parents) - 6
+
+            if extension not in ['py', 'json']:
+                continue
+            
+            f.write(f'\subsection{{{file}}}\n')
+            
             if file.split('.')[1] == 'py':
                 f.write(f'\lstinputlisting{{{file_path}}}\n')
             
