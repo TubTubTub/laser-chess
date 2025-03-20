396daaccba03826fa5c04e556637176dcfc6b389
TubTubTub <tobykwmok@gmail.com>
2025-03-20 09:14:17 +0000

    s

--- add_init.py
+++ add_init.py
@@ -1,6 +1,6 @@
 import pathlib
 
-for one, two, three in pathlib.Path('test').walk():
+for one, two, three in pathlib.Path('data').walk():
     if '__init__.py' not in three:
-        with open('__init__.py', 'w') as f:
+        with open((one / '__init__.py').resolve(), 'w') as f:
             f.write('')
